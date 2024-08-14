8122dd78e7bb90ccc27fcc050157d7e372f63597
BhargavPatel29 <bkpatel0111@gmail.com>
2024-08-14 21:21:48 +0530

    Initial commit

---
+++ app/src/main/java/in/qeasy/bkutils/MainActivity.java
@@ -0,0 +1,28 @@
+package in.qeasy.bkutils;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import in.qeasy.bktoast.BKToast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        BKToast.makeText(this, "Hello World", BKToast.LENGTH_SHORT, BKToast.TYPE_SUCCESS, BKToast.GRAVITY_TOP_RIGHT).show();
+    }
+}
